2be1d1bbaa938ca013a9ab1614e42c75ac0a2f15
Luis Thaylor dos Santos de Mello <luis.thaylor2018@gmail.com>
2024-06-22 22:03:10 -0300

    FEITO LAYOUT BASICO, POREM AINDA SEM LINHAS ENTRE ESTRELAS

--- main.py
+++ main.py
@@ -1,47 +1,93 @@
 #NOME DOS PARTICIPANTES --> LUIS THAYLOR/EMANOEL ROSA
 
-
-
 import pygame
-import random
-import os
-from tkinter import simpledialog
+from tkinter import simpledialog, messagebox
+import math
 
-tamanho = (800, 600)
+pygame.init()
+
+tamanho = (800,600)
 clock = pygame.time.Clock()
-tela=pygame.display.set_mode(tamanho)
-pygame.display.set_caption('Game Titulo')
-
-
-branco = (255,255,255)
+tela = pygame.display.set_mode(tamanho)
+pygame.display.set_caption("SpaceMaker")
+fundo = pygame.image.load("assets/fundo.jpg")
+icone = pygame.image.load("assets/space.png")
+fonte = pygame.font.SysFont("Arial", 18)
+som = pygame.mixer.Sound("assets/somgame.mp3")
+som.play(-1)
+estrelas = {}
+raio = 5
+branco = (225,225,225)
 preto = (0,0,0)
-vermelho=(255,0,0)
 
 
 
-posiçãoXBolinha= 0
+def salvarMarcacoes():
+     with open("Estralas Marcadas.txt", "w") as arquivo:
+          for nome, posicao in estrelas.items():
+               arquivo.write(f"{posicao[0]},{posicao[1]},{nome}\n")
+
+def carregarMarcacoes():
+     estrelas.clear()
+     try:
+          with open("Estrelas Marcadas.txt", "r") as arquivo:
+               for linha in arquivo:
+                    try:
+                         x, y, nome = linha.strip().split(",")
+                         posicao = (int(x), int(y))
+                         estrelas[nome] = posicao
+                    except ValueError:
+                         continue
+     except FileNotFoundError:
+          messagebox.showinfo("Erro", "Arquivo não encontrado.")     
+
+
+def excluirMarcacoes():
+     estrelas.clear()
+
+
 while True:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            quit()
+     for evento in pygame.event.get():
+          if evento.type == pygame.QUIT:
+               quit()
+               salvarMarcacoes()
+          elif evento.type == pygame.KEYDOWN:
+               if evento.key ==pygame.K_ESCAPE:
+                    pygame.quit()
+                    salvarMarcacoes()
+               elif evento.key == pygame.K_F10:
+                    salvarMarcacoes()
+               elif evento.key == pygame.K_F11:
+                    carregarMarcacoes()
+               elif evento.key == pygame.K_F12:
+                    excluirMarcacoes()
+          elif evento.type == pygame.MOUSEBUTTONUP:
+               pos = pygame.mouse.get_pos()
+               item = simpledialog.askstring("Space", "Nome da Estrela: ")
+               print(item)
+               if item == None:
+                    item = "desconhecido"+str(pos)
+               estrelas[item] = pos
 
+     tela.fill(branco)
+     tela.blit(fundo,(0,0))
 
+     for item, posicoes in estrelas.items():
+        pygame.draw.circle(tela, branco, posicoes, raio)
+        fonte = pygame.font.Font(None, 20)
+        texto = fonte.render(item, True, (255, 255, 255))
+        tela.blit(texto, (posicoes[0] + 10, posicoes[1] + 10))
 
+     textoOpcoes = fonte.render("Opções:", True, branco)
+     textoSalvar = fonte.render("F10 - Salvar marcações", True, branco)
+     textoCarregar = fonte.render("F11 - Carregar ultimas marcações", True, branco)
+     textoExcluir = fonte.render("F12 - Excluir todas as marcações", True, branco)
+     tela.blit(textoOpcoes, (10, 10))
+     tela.blit(textoSalvar, (10, 40))
+     tela.blit(textoCarregar, (10, 70))
+     tela.blit(textoExcluir, (10, 100))
 
+     pygame.display.update()
+     clock.tick(60)
 
-    tela.fill(branco)
-
-    pygame.draw.line(tela,preto,(80,95), (200,400) , 3)
-    pygame.draw.circle(tela,preto,(70,70),30,3)
-    pygame.draw.circle(tela,preto,(205,430),30,3)
-
-    #pygame.draw.rect(tela,preto,(20,50,50,100),2)
-
-
-
-    
-    pygame.display.update()
-    clock.tick(60)
-
-
-
+pygame.quit()
